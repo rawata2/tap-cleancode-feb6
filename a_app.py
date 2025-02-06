e001ce7c024b5ace1b38eb408e516ca6a3b738f0
Arvind <72453093+rawata2@users.noreply.github.com>
2025-02-06 19:04:35 +0000

    new code

---
+++ a_app.py
@@ -0,0 +1,47 @@
+# ---- Session on Clean Coding, Data types, Functions, TDD ----
+horizontal_rule = "-" * 40
+
+# Refactor the following
+
+# Assigning Lists
+list_sample = [1, 2, 3, "Four", 5, 6]
+
+# Access Lists
+print("     Item 1:", list_sample[0])
+print("     Item 4:", list_sample[3])
+print("  Item Last:", list_sample[-1])
+print("Slice items:", list_sample[2:len(list_sample)],f'array length: {list_sample}')
+# Loop through Lists
+print(" ---- Listing Items in List ----")
+for item in list_sample:
+  print(item)
+print(" ---- Listing Items using range() ----")
+# for using range(start,stop,step) / range(stop)
+for ctr in range(len(list_sample)):
+  print(ctr, ":", list_sample[ctr])
+print(horizontal_rule)
+
+em = "j@em2.com"
+
+# Dictionary : { key_1: value_1, key_2: value_2, ..... key_n: value_n }
+dictionary_sample = {
+  "id" : 123,
+  "name": "John",
+  "email" : [
+      "j@em.com",
+      em
+  ]
+}
+
+# Access dictionaries individually
+print("   ---- Access Dictionaries ----")
+print("     Id:")
+print("   Name:")
+print(" Emails:")
+print("Email 1:")
+print("Email 2:")
+print(horizontal_rule)
+# Loop through dictionaries print key : value - element type
+# for
+print("-"*20)
+
